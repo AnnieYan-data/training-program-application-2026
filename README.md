1268a2e13803040be20d288a6e6e1ec17f3473e0
egmg726 <email.egail@gmail.com>
2026-01-05 12:09:52 +1100

    expanded instructions

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ Perform a simple exploratory analysis of RNA-seq expression data and associated 
 1. Fork this repository
 2. Complete the code in analysis.R
 3. Save your plot to the results/ directory
-4. Commit the changes to the with a meaningful
-5. Complete the questions in the application form
+4. Commit the changes to repository the with a meaningful comment.
+5. Complete the associated questions in the application form.
 
--- analysis.r
+++ analysis.r
@@ -1,17 +1,31 @@
+# ---------------------------------------------------------
+
 # Melbourne Bioinformatics Training Program
 
+# This exercise to assess your familiarity with R and git. Please follow
+# the instructions on the README page and link to your repo in your application.
+# If you do not link to your repo, your application will be automatically denied.
 
+# You can use the resources available on our training website for help:
+# Intro to R: https://mbite.org/intro-to-r
+# Version Control with Git: https://mbite.org/intro-to-git/
+
+# ----------------------------------------------------------
 
 # Load libraries -------------------
 # You may use base R or tidyverse for this exercise
 
+# ex. library(tidyverse)
 
 # Load data here ----------------------
+# Load each file with a meaningful variable name.
+
 
 
 # Inspect the data -------------------------
 
-# What are the dimensions of each dataset? (How many rows/columns in each?)
+# What are the dimensions of each data set? (How many rows/columns in each?)
+# Keep the code here for each file.
 
 ## Expression data
 
@@ -19,8 +33,15 @@
 ## Metadata
 
 
+# Prepare/combine the data for plotting ------------------------
+# How can you combine this data
+
+
+
 # Plot the data --------------------------
 ## Plot the expression by cell type
+## Can use boxplot() or geom_boxplot() in ggplot2
+
 
 
 ## Save the plot
